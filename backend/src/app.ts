786f638632068ce129684ef3faa7bbd40e8abe81
abdoulore <ore.abddulquadri@gmail.com>
2026-06-10 00:06:52 +0100

    fixed image upload

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,6 +17,8 @@ import uploadRoutes from './modules/uploads/upload.routes';
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 app.use(helmet());
 app.use(cors({ origin: env.CLIENT_URL, credentials: true }));
 app.use(express.json());
